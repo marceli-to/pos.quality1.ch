fd2fc91e0538c4d12ca8546b6849a7b6dfc19882
Marcel Stadelmann <m@marceli.to>
2023-07-03 11:15:24 +0200

    wip

--- resources/views/web/pages/index.blade.php
+++ resources/views/web/pages/index.blade.php
@@ -19,16 +19,16 @@
       <img src="/assets/img/keyvisual.png" height="1920" width="1920" alt="{{__('Quality1 mit neuem Auftritt')}}">
     </figure>
     <p>{{__('Seit 21 Jahren sind Fahrzeuggarantien unsere Leidenschaft, und wir sind dankbar, dass wir viele von Ihnen als langjährige Partner an unserer Seite haben.') }}</p>
-    <p>{!!__('Um Ihnen weiterhin das Beste bieten zu können, entwickeln wir unsere Angebote ständig weiter. Wir haben unseren Quality1-Auftritt vereinheitlicht und einen frischen Wind in unsere Firmenpräsentation gebracht. Verschiedene historische Marken wurden zu einer gemeinsamen Marke, <strong>Quality1</strong>, gebündelt und unser POS-Material haben wir entsprechend aktualisiert, weiterentwickelt und aufgefrischt.') !!}</p>
+    <p>{!!__('Um Ihnen weiterhin das Beste bieten zu können, entwickeln wir unsere Angebote ständig weiter. Wir haben unseren Auftritt als Quality1 vereinheitlicht. Verschiedene historische Marken wurden in der gemeinsamen Marke <strong>Quality1</strong> gebündelt. Unser POS-Material haben wir entsprechend aktualisiert, weiterentwickelt und aufgefrischt. ') !!}</p>
     <div class="landing-intro__list">
       <p><strong>{{ __('Im Detail bedeutet das:') }}</strong></p>
       <ul>
         <li>{{ __('Die Flyer wurden überarbeitet und sind bereits im Einsatz.') }}</li>
-        <li>{{ __('Die Nummernschilder sowie Fahnen wurden entsprechend dem neuen Design erstellt und sind bereits erhältlich.') }}</li>
+        <li>{{ __('Die Nummernschilder sowie Fahnen wurden entsprechend dem neuen Design erstellt und sind ebenfalls bereits erhältlich.') }}</li>
         <li>{{ __('Das Quality1-Logo kann bei AutoScout24 als Qualitätssiegel angezeigt werden.') }}</li>
       </ul>
     </div>
-    <p>{!! __('Die optimale Wiedererkennung der Marke Quality1 bei Fahrzeughaltern ist unser Ziel. Davon profitiert auch Ihre Firma. Wir sind überzeugt, dass eine einheitliche Markenpräsenz zu einer klareren Positionierung führt.') !!}</p>
+    <p>{!! __('So optimieren wir den Wiedererkennungswert der Marke Quality1 bei Fahrzeughaltern – das ist unser Ziel. Davon profitiert auch Ihre Firma. Wir sind überzeugt, dass eine einheitliche Markenpräsenz zu einer klareren Positionierung führt. ') !!}</p>
   </div>
 </section>
 <section class="landing-order">
@@ -43,7 +43,7 @@
 <section class="landing-participate">
   <div>
     <h2>{!! __('Jetzt mitmachen') !!}</h2>
-    <p>{!! __('Wir freuen uns darauf, den überarbeiteten Auftritt von Quality1 gemeinsam mit Ihnen umzusetzen und unseren Kundinnen und Kunden zu präsentieren. Zeigen Sie uns bis zum <strong>17. September 2023</strong> Fotos Ihres Betriebs, wie Sie die neu gestalteten POS-Materialien, insbesondere die Nummernschilder und Fahnen, einsetzen.') !!}</p>
+    <p>{!! __('Wir freuen uns darauf, den vereinheitlichten Auftritt von Quality1 gemeinsam mit Ihnen umzusetzen und unseren Kundinnen und Kunden zu präsentieren. Zeigen Sie uns bis zum <strong>27. Oktober 2023</strong> mit Fotos von Ihrem Betrieb, wie Sie die neu gestalteten POS-Materialien, insbesondere die Nummernschilder und Fahnen, einsetzen.') !!}</p>
     <p>{!! __('Am 20. September 2023 werden wir aus den eingesendeten Bildern <strong>21&nbsp;Gewinner auslosen</strong>. Als Dankeschön für die aktive Unterstützung erhalten die Gewinner eine Überraschung.') !!}</p>
     <p>{!! __('Wir freuen uns auf Ihre Teilnahme und stehen Ihnen bei Fragen gerne zur Verfügung!') !!}</p>
   </div>
